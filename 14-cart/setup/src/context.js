3ce925b15c641ca3d04891f2e5f777045a25611e
techshahnawaz011 <md.shahanawaz@cybercrescent.tech>
2024-08-22 10:40:01 +0530

    add some projects

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -1,32 +1,63 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react'
-import cartItems from './data'
-import reducer from './reducer'
+import React, { useState, useContext, useReducer, useEffect } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-useReducer-cart-project'
-const AppContext = React.createContext()
-const initialState ={
-  loading:false,
-  cart:cartItems,
-  amount:0,
-  total:0
-}
+const url = "https://course-api.com/react-useReducer-cart-project";
+const AppContext = React.createContext();
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  amount: 0,
+  total: 0,
+};
 const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer,initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+  const remove = (id) => {
+    dispatch({ type: "REMOVE", payload: id });
+  };
+  const increase = (id) => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+  const decrease = (id) => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
+
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+  }, [state.cart]);
+
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+  //  useEffect(() => {
+  //    fetchData();
+  //  }, []);
   return (
     <AppContext.Provider
       value={{
-        ...state
+        ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+        fetchData,
       }}
     >
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
--- 14-cart/setup/src/data.js
+++ 14-cart/setup/src/data.js
@@ -20,4 +20,25 @@ export default [
     img: "https://i05.appmifile.com/735_item_uk/16/02/2024/1190124a8dd202366319e1298d637a63!400x400!85.png",
     amount: 1,
   },
+  {
+    id: 4,
+    title: "Samsung Galaxy S7",
+    price: 599.99,
+    img: "https://static.doji.co/product/medias/smartphones/samsung/galaxy-s7-edge/1-white-front_back.png",
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: "google pixel ",
+    price: 499.99,
+    img: "https://storage.googleapis.com/alpine-inkwell-325917.appspot.com/devices/pixel-6-header.png",
+    amount: 1,
+  },
+  {
+    id: 6,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img: "https://i05.appmifile.com/735_item_uk/16/02/2024/1190124a8dd202366319e1298d637a63!400x400!85.png",
+    amount: 1,
+  },
 ];
